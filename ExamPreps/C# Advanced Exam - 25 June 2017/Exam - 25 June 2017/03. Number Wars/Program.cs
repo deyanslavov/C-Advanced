7e361fd84f1a4954ccb1afdc97101d6345d17bb3
deyanslavov <pikacho2013@gmail.com>
2018-02-12 18:26:53 +0200

    Added the exam on 11 February again

---
+++ ExamPreps/C# Advanced Exam - 25 June 2017/Exam - 25 June 2017/03. Number Wars/Program.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Number_Wars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string alphabet = "0abcdefghijklmnopqrstuvwxyz";
+            var firstHand = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var secondHand = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Queue<string> firstPlayerCards = new Queue<string>(firstHand);
+            Queue<string> secondPlayerCards = new Queue<string>(secondHand);
+            int turnsCount = 0;
+            bool wasThereAWar = false;
+            bool firstPWin = false;
+            bool secondPWin = false;
+            bool isDraw = false;
+            while (firstPlayerCards.Count > 0 && secondPlayerCards.Count > 0 && turnsCount <= 1000000)
+            {
+                turnsCount++;
+                var currHand1 = firstPlayerCards.Dequeue();
+                var currHand2 = secondPlayerCards.Dequeue();
+
+                int firstNum = int.Parse(currHand1.Substring(0, currHand1.Length - 1));
+                int secondNum = int.Parse(currHand2.Substring(0, currHand2.Length - 1));
+                if (firstNum > secondNum)
+                {
+                    firstPlayerCards.Enqueue(currHand1);
+                    firstPlayerCards.Enqueue(currHand2);
+                }
+                else if (secondNum > firstNum)
+                {
+                    secondPlayerCards.Enqueue(currHand2);
+                    secondPlayerCards.Enqueue(currHand1);
+                }
+                else
+                {
+                    wasThereAWar = true;
+                    if (secondPlayerCards.Count < 3 && firstPlayerCards.Count < 3)
+                    {
+                        isDraw = true;
+                        break;
+                    }
+                    else if (firstPlayerCards.Count < 3)
+                    {
+                        secondPWin = true;
+                        break;
+                    }
+                    else if (secondPlayerCards.Count < 3)
+                    {
+                        firstPWin = true;
+                        break;
+                    }
+                    else
+                    {
+                        List<string> cards = new List<string>();
+                        int firstSum = GetSum(alphabet, firstPlayerCards, cards);
+                        int secondSum = GetSum(alphabet, secondPlayerCards, cards);
+                        cards = cards.OrderByDescending(a => a).ToList();
+
+                        if (firstSum > secondSum)
+                        {
+                            for (int i = 0; i < cards.Count; i++)
+                            {
+                                firstPlayerCards.Enqueue(cards[i]);
+                            }
+                            cards.Clear();
+                        }
+                        else if(secondSum > firstSum)
+                        {
+                            for (int i = 0; i < cards.Count; i++)
+                            {
+                                secondPlayerCards.Enqueue(cards[i]);
+                            }
+                            cards.Clear();
+                        }
+                        else
+                        {
+                            bool gameEnded = false;
+                            while (true)
+                            {
+                                if (firstSum == secondSum)
+                                {
+                                    if (secondPlayerCards.Count < 3 && firstPlayerCards.Count < 3)
+                                    {
+                                        gameEnded = true;
+                                        isDraw = true;
+                                        break;
+                                    }
+                                    else if (firstPlayerCards.Count < 3)
+                                    {
+                                        gameEnded = true;
+                                        secondPWin = true;
+                                        break;
+                                    }
+                                    else if (secondPlayerCards.Count < 3)
+                                    {
+                                        gameEnded = true;
+                                        firstPWin = true;
+                                        break;
+                                    }
+                                    firstSum += GetSum(alphabet, firstPlayerCards, cards);
+                                    secondSum += GetSum(alphabet, secondPlayerCards, cards);
+                                }
+                                else
+                                {
+                                    if (firstSum > secondSum)
+                                    {
+                                        firstPWin = true;
+                                        gameEnded = true;
+                                        break;
+                                    }
+                                    else if (secondSum > firstSum)
+                                    {
+                                        secondPWin = true;
+                                        gameEnded = true;
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        isDraw = true;
+                                        gameEnded = true;
+                                        break;
+                                    }
+                                }
+                            }
+                            if (gameEnded)
+                            {
+                                break;
+                            }
+                            cards.OrderByDescending(a => a).ToList();
+                            if (firstSum > secondSum)
+                            {
+                                for (int i = 0; i < cards.Count; i++)
+                                {
+                                    firstPlayerCards.Enqueue(cards[i]);
+                                }
+                                cards.Clear();
+                            }
+                            else if (secondSum > firstSum)
+                            {
+                                for (int i = 0; i < cards.Count; i++)
+                                {
+                                    secondPlayerCards.Enqueue(cards[i]);
+                                }
+                                cards.Clear();
+                            }
+                        }
+                    }
+                }
+            }
+            if (wasThereAWar == false)
+            {
+                if (firstPlayerCards.Count == 0)
+                {
+                    Console.WriteLine($"Second player wins after {turnsCount} turns");
+                }
+                else if (secondPlayerCards.Count == 0)
+                {
+                    Console.WriteLine($"First player wins after {turnsCount} turns");
+                }
+                else if (firstPlayerCards.Count == 0 && secondPlayerCards.Count == 0)
+                {
+                    Console.WriteLine($"Draw after {turnsCount} turns");
+                }
+            }
+            else
+            {
+                if (isDraw)
+                {
+                    Console.WriteLine($"Draw after {turnsCount} turns");
+                }
+                else if (secondPWin)
+                {
+                    Console.WriteLine($"Second player wins after {turnsCount} turns");
+                }
+                else if (firstPWin)
+                {
+                    Console.WriteLine($"First player wins after {turnsCount} turns");
+                }
+            }
+        }
+
+        private static int GetSum(string alphabet, Queue<string> firstPlayerCards, List<string> cards)
+        {
+            int sum = 0;
+
+            var player1card1 = firstPlayerCards.Dequeue();
+            var player1card1Value = player1card1[player1card1.Length - 1];
+            var player1card2 = firstPlayerCards.Dequeue();
+            var player1card2Value = player1card2[player1card2.Length - 1];
+            var player1card3 = firstPlayerCards.Dequeue();
+            var player1card3Value = player1card3[player1card3.Length - 1];
+            cards.Add(player1card1); cards.Add(player1card2); cards.Add(player1card3);
+
+            return sum = alphabet.IndexOf(player1card1Value) + alphabet.IndexOf(player1card2Value) + alphabet.IndexOf(player1card3Value);
+        }
+    }
+}
---
+++ ExamPreps/C# Advanced Exam - 25 June 2017/Exam - 25 June 2017/04. Hospital With Classes/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Hospital_With_Classes
+{
+    class Doctor
+    {
+        public string Name { get; set; }
+        public List<string> Patients { get; set; }
+
+        public Doctor(string name, List<string> patients)
+        {
+            this.Name = name;
+            this.Patients = patients;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hospital = new Dictionary<string, List<Doctor>>();
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                if (input == "Output")
+                {
+                    var output = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (output.Length == 1)
+                    {
+                        foreach (var item in hospital[output[0]])
+                        {
+                            foreach (var dp in item.Patients)
+                            {
+                                Console.WriteLine(dp);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        int room = 0;
+                        if (int.TryParse(output[1], out room))
+                        {
+                            var printList = new List<string>();
+                            foreach (var item in hospital[output[0]])
+                            {
+                                printList.AddRange(item.Patients);
+                            }
+                            if (printList.Count - 1 < room * 3 - 3)
+                            {
+                                continue;
+                            }
+                            var result = new List<string>(3);
+                            for (int i = room * 3 - 3; i < room * 3; i++)
+                            {
+                                result.Add(printList[i]);
+                            }
+                            result = result.OrderBy(a => a).ToList();
+                            foreach (var patient in result)
+                            {
+                                Console.WriteLine(patient);
+                            }
+                        }
+                        else
+                        {
+                            var doc = output[0] + " " + output[1];
+                            var patientsResult = new List<string>();
+                            var r = hospital.Values.Where(a => a.Any(b => b.Name == doc));
+                            foreach (var item in hospital.Values.Where(a => a.Any( b => b.Name == doc)))
+                            {
+                                foreach (var wabamama in item)
+                                {
+                                    patientsResult.AddRange(wabamama.Patients);
+                                }
+                            }
+                            patientsResult = patientsResult.OrderBy(a => a).ToList();
+                            foreach (var item in patientsResult)
+                            {
+                                Console.WriteLine(item);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    var tokens = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    var department = tokens[0];
+                    var doctor = tokens[1] + " " + tokens[2];
+                    var patient = tokens[3];
+                    var doc = new Doctor(doctor, new List<string>()) { Name = doctor, Patients = new List<string>() };
+
+
+                    if (!hospital.ContainsKey(department))
+                    {
+                        hospital.Add(department, new List<Doctor>());
+                    }
+                    if (hospital.ContainsKey(department))
+                    {
+                        int sum = 0;
+                        foreach (var dep in hospital[department])
+                        {
+                            sum += dep.Patients.Count;
+                        }
+                        if (sum < 60)
+                        {
+                            if (!hospital[department].Any(a => a.Name == doc.Name))
+                            {
+                                hospital[department].Add(doc);
+                                doc.Patients.Add(patient);
+                            }
+                            else
+                            {
+                                doc.Patients.Add(patient);
+                            }
+                        }
+                    }
+                }  
+            }
+        }
+    }
+}
