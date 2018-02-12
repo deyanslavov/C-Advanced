f0a34c063df7f2131dd82117c888b705e4ce4141
deyanslavov <pikacho2013@gmail.com>
2018-02-12 18:18:17 +0200

    Added Exam 11 February 2018

---
+++ Exam - 11 February 2018/Problem 3/Problem 3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Problem_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+            StringBuilder output = new StringBuilder();
+            Regex numbersRegex = new Regex(@"\[[^\d]*(?<nums>\d+)[^\d]*\]|\{[^\d]*(?<nums>\d+)[^\d]*\}");
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine();
+                sb.Append(input);
+            }
+            var matches = numbersRegex.Matches(sb.ToString());
+            foreach (Match item in matches)
+            {
+                var nums = item.Groups["nums"].Value;
+                if (nums.Length % 3 == 0)
+                {
+                    for (int i = 0; i < nums.Length; i+=3)
+                    {
+                        var number = (char)((int.Parse(nums.Substring(i, 3)) - item.Length));
+                        output.Append(Convert.ToChar(number));
+                    }
+                }
+            }
+            Console.WriteLine(output.ToString());
+        }
+    }
+}
---
+++ Exam - 11 February 2018/Problem 4/Problem 4.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new Dictionary<string, Dictionary<string, string>>();
+
+            int infoIndex = int.Parse(Console.ReadLine());
+
+            string line;
+            while ((line = Console.ReadLine()) != "end transmissions")
+            {
+                var nameToken = line.Split(new[] { '=', ';', ':' }, StringSplitOptions.RemoveEmptyEntries);
+                string name = nameToken[0];
+                if (!data.ContainsKey(name))
+                {
+                    data.Add(name, new Dictionary<string, string>());
+                }
+                string[] otherData = nameToken.Skip(1).ToArray();
+                for (int i = 0; i < otherData.Length; i+=2)
+                {
+                    var key = otherData[i];
+                    var value = otherData[i+1];
+                    if (!data[name].ContainsKey(key))
+                    {
+                        data[name].Add(key, value);
+                    }
+                    else
+                    {
+                        data[name][key] = value;
+                    }
+                }
+            }
+            var killCommand = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine($"Info on {killCommand[1]}:");
+            var sum = 0;
+            foreach (var kvp in data[killCommand[1]].OrderBy(a => a.Key))
+            {
+                sum += kvp.Key.Length + kvp.Value.Length;
+                Console.WriteLine($"---{kvp.Key}: {kvp.Value}");
+            }
+            Console.WriteLine($"Info index: {sum}");
+            if (sum >= infoIndex)
+            {
+                Console.WriteLine("Proceed");
+            }
+            else
+            {
+                Console.WriteLine($"Need {infoIndex - sum} more info.");
+            }
+        }
+    }
+}
