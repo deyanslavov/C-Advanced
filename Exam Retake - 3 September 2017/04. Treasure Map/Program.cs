cecc85a731af2f236f8dd00a2f6dc05ffb5f8b4a
deyanslavov <pikacho2013@gmail.com>
2018-04-30 21:36:37 +0300

    added Exam retake from 3th September 2017

---
+++ Exam Retake - 3 September 2017/04. Treasure Map/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace _04.Treasure_Map
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+            string streetNamePattern = @"([[a-zA-Z]{4})[^a-zA-Z0-9]";
+            string streetNumberPattern6 = @"(\d{3}\-\d{6})";
+            string streetNumberPattern4 = @"(\d{3}\-\d{4})";
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine();
+                var startIndex = 0;
+                var endIndex = 0;
+                var listIndexes = new List<int[]>();
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    if (input[j] == '#' || input[j] == '!')
+                    {
+                        startIndex = j;
+                        if (input[startIndex] == '#')
+                        {
+                            for (int k = startIndex+1; k < input.Length; k++)
+                            {
+                                if (input[k] == '!')
+                                {
+                                    endIndex = k;
+                                    break;
+                                }
+                                else if (input[k] == '#')
+                                {
+                                    startIndex = k;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            for (int k = startIndex+1; k < input.Length; k++)
+                            {
+                                if (input[k] == '#')
+                                {
+                                    endIndex = k;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
